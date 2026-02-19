7cb7e6cb1cc39a59d331f1cec8099e19f6b6b610
Yamii <73439412+Yamoteusz@users.noreply.github.com>
2026-02-19 20:42:17 +0100

    Progressive Web App - Android & IOS

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'rebels-club-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/images/uploads/rebels.png'
+];
+
+// Instalacja Service Workera i zapisanie plików w cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Zwracanie plików z cache, jeśli użytkownik jest offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Zwróć z pamięci podręcznej
+        }
+        return fetch(event.request); // Pobierz z sieci
+      })
+  );
+});
